860908432d06ffd000b77d6bd6a70e7aad76a931
felixdrp <felixdrp@gmail.com>
2024-12-09 20:30:32 +0000

    refactor: verbose code

--- example.js
+++ example.js
@@ -1,2 +1,5 @@
 const example = require('./dist/lib.node');
-console.log(example.foo((text1, text2)=>{console.log(text1 + text2)}));
+const callThisFunction = (text1, text2)=> console.log(text1 + text2);
+// Call callThisFunction and returns text1
+const result = example.foo(callThisFunction);
+console.log(result);
